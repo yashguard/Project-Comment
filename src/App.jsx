47b5324f48d664ad1ad4397dbbc3b398b6fc8bce
yashguard <yashguard2002@gmail.com>
2023-04-14 20:16:44 +0530

    Cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,69 @@
-import React, {useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
-import {Cart} from './Components/Cart'
+import { Cart } from './Components/Cart'
 
 function App() {
-  let [data,setData] = useState([])
+  let [comments,setComments] = useState()
+  let [data, setData] = useState([])
+  useEffect(() => {
+    apiArray();
+  }, [])
   let apiArray = async () => {
     let res = await fetch(`https://dummyjson.com/products?pages=1&limit=16`);
     let req = await res.json();
-    // setData(req.products)
-    console.log(req.products)
+    setData(req.products)
   }
-  apiArray()
+  let fname = useRef()
+  let lname = useRef()
+  let email = useRef()
+  let comment = useRef("")
+  const submit = (e) => {
+    e.preventDefault();
+    setComments(comment.current)
+    console.log(comments)
+    if (fname.current.value === "") {
+      alert("Please write your name.");
+      fname.current.focus()
+    }
+    else if (lname.current.value === "") {
+      alert("Please write your surname.");
+      lname.current.focus()
+    }
+    else if (email.current.value === "") {
+      alert("Please write your surname.");
+      email.current.focus()
+    }
+    else if (comment.current.value === "") {
+      alert("Please write your surname.");
+      comment.current.focus()
+    }
+    else {
+      alert("Comment sent succefully")
+    }
+    fname.current.value = "";
+    lname.current.value = "";
+    email.current.value = "";
+    comment.current.value = "";
+    fname.current.focus();
+  }
   return (
     <>
-      <Cart />
+      {data.map((v,i) => {
+        return <Cart key={i} {...v} comment={comments} />
+      })}
+      <section className='comment-section'>
+        <div className="container">
+          <form onSubmit={submit}>
+            <div className="row1">
+              Enter your First Name : <input type="text" className='text' ref={fname} />
+              Enter your Last Name : <input type="text" className='text' ref={lname} />
+              Enter your Email : <input type="email" className='text' ref={email} />
+            </div>
+            Enter your comments : <input type="text" className='email' ref={comment} />
+            <input type="submit" value="Submit" className='submit' />
+          </form>
+        </div>
+      </section>
     </>
   );
 }
--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
 
-export const Cart = () => {
-  return (
-    <div>
-      <div className="container">
-         <div className="row">
-            <div className="image">
-               <img src="https://i.dummyjson.com/data/products/1/thumbnail.jpg" />
-               <button>Add to Cart</button>
-               <button>Buy Now</button>
-            </div>
-            <div className="content">
-               <h1 className="title">iPhone 9</h1>
-               <p className="description">An apple mobile which is nothing like apple</p>
-               <span className="price">549$</span>
-               <span className="disc">12.96 %off</span>
-               <div className="rating">4.69<i class="fa-solid fa-star"></i></div>
-               <div className="brand">Apple</div>
-               <div className="category">smartphones</div>
-               <button>Comments</button>
-               <div className="comment-box"></div>
+export const Cart = (props) => {
+   return (
+      <div>
+         <div className="container">
+            <div className="row">
+               <div className="image">
+                  <img src={props.thumbnail} alt='' />
+                  <button>Add to Cart</button>
+                  <button>Buy Now</button>
+               </div>
+               <div className="content">
+                  <h1 className="title">{props.title}</h1>
+                  <p className="description">{props.description}</p>
+                  <span className="price">{props.price}$</span>
+                  <span className="disc">{props.discount} %off</span>
+                  <div className="rating">{props.rating}<i class="fa-solid fa-star"></i></div>
+                  <div className="brand">{props.brand}</div>
+                  <div className="category">{props.category}</div>
+                  <p>{props.comment}</p>
+               </div>
             </div>
          </div>
       </div>
-    </div>
-  )
+   )
 }
